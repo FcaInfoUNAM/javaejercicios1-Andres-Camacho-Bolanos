d480d8f36222c8e6ea549d5ebe780f32e0b16636
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2024-08-24 01:11:51 +0000

    Se crearon archivos separados para las clases alumno y profesor

---
+++ Exercises/Alumno.java
@@ -0,0 +1,17 @@
+/**
+     * Alumno
+     */
+    class Alumno extends Sobrecarga{
+        public Alumno(String nombre, String materia, int calificacion){
+            super(nombre, materia, calificacion);
+        }
+
+        public String reprobar(){
+        
+            if (this.calificacion<6)
+            return ":(";
+        else
+            return ":)";
+            
+    }
+        }
---
+++ Exercises/Profesor.java
@@ -0,0 +1,14 @@
+/**
+     * Profesor
+     */
+    public class Profesor extends Sobrecarga{
+        public Profesor(String nombre, String materia, int calificacion){
+            super(nombre, materia, calificacion);
+        }
+        public String reprobar(){
+            if(this.calificacion>5)
+                return ">:) Sacaste "+this.calificacion+" en " + this.materia;
+            else
+                return ":(";
+        }
+    }
