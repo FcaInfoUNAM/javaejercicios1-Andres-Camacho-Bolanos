c0d3ca7a14a640d0abb21a3225aca9d6ef9c31dc
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2024-08-24 00:52:19 +0000

    Se creó un método reprobar alumno que evalúe que el parámetro recibido en calificación sea mayor a 6

--- Exercises/Sobrecarga.java
+++ Exercises/Sobrecarga.java
@@ -41,8 +41,15 @@ public class Sobrecarga {
         public Alumno(String nombre, String materia, int calificacion){
             super(nombre, materia, calificacion);
         }
-        public String reprobar(){
 
+        public String reprobar(){
+        
+            if (this.calificacion<6)
+            return ":(";
+        else
+            return ":)";
+            
+    }
         }
         
     }
