bedc83c84ded04ba9f49cb192d69718f23061aef
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2024-08-24 01:43:53 +0000

    Se corrigieron errores

--- Exercises/Alumno.java
+++ Exercises/Alumno.java
@@ -1,3 +1,4 @@
+package Exercises;
 /**
      * Alumno
      */
--- Exercises/DataTypes.java
+++ Exercises/DataTypes.java
@@ -8,9 +8,27 @@ public class DataTypes {
     boolean boleano;
 
 
+public String Cadena(){
+return cadena;
+}
 
+public int entero(){
+    return entero;
+}
 
-public DataTypes(){
+public float flotante(){
+    return flotante;
+}
+
+public char caracter(){
+    return caracter;
+}
+
+public boolean boleano(){
+    return boleano;
+}
+
+public void DataTypes(String cadena, int entero, float flotante, char caracter, boolean boleano){
     this.cadena = "Hola mundo";
     this.entero = 10;
     this.flotante = 5.9f;
